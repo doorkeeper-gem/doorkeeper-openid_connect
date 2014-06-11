f8147fc95bd757d2b704840bc1b8566019b7a40a
Sam Dengler <sam.dengler@playonsports.com>
2014-06-11 17:42:29 -0400

    modified Userinfo controller to user doorkeeper token

--- app/controllers/doorkeeper/openid_connect/userinfo_controller.rb
+++ app/controllers/doorkeeper/openid_connect/userinfo_controller.rb
@@ -2,7 +2,14 @@ module Doorkeeper
   module OpenidConnect
     class UserinfoController < ::Doorkeeper::ApplicationController
       def show
-        render text: 'hello world'
+        if doorkeeper_token && doorkeeper_token.accessible?
+          render json: { sub: doorkeeper_token.resource_owner_id },
+                 status: :ok
+        else
+          error = OAuth::ErrorResponse.new(name: :invalid_request)
+          response.headers.merge!(error.headers)
+          render json: error.body, status: error.status
+        end
       end
     end
   end
--- lib/doorkeeper/openid_connect/engine.rb
+++ lib/doorkeeper/openid_connect/engine.rb
@@ -1,7 +1,6 @@
 module Doorkeeper
   module OpenidConnect
     class Engine < ::Rails::Engine
-      puts '************ Doorkeeper::OpenidConnect::Engine'
       initializer 'doorkeeper.openid_connect.routes' do
         Doorkeeper::OpenidConnect::Rails::Routes.install!
       end
