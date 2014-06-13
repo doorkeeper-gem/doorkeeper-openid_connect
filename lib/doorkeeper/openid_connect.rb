66657e40320ce739a52855a0ccbc18d9835e4bb5
Sam Dengler <sam.dengler@playonsports.com>
2014-06-13 17:10:39 -0400

    stub out some methods for generating id_token in response

--- lib/doorkeeper/openid_connect.rb
+++ lib/doorkeeper/openid_connect.rb
@@ -23,6 +23,9 @@ module Doorkeeper
 
       def after_successful_response
         puts "*** #{Doorkeeper::OpenidConnect.configuration.jws_key} ***"
+        # subject = User.find(doorkeeper_token.resource_owner_id).key
+        # id_token = IdToken.new(subject)
+        # @response.id_token = id_token.to_jws_token
         @response.id_token = 'foo'
       end
     end
