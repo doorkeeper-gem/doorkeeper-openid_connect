f15182fc8a2cca4657cac023e9ac2ae5f44685c7
Nicholas Hance <nhance@reenhanced.com>
2017-12-27 17:05:40 -0500

    Creates an extension for Doorkeeper::Request

---
+++ lib/doorkeeper/request.rb
@@ -0,0 +1,16 @@
+module Doorkeeper
+    module Request
+      class << self
+        def get_strategy_with_multitype(grant_or_request_type, available)
+          grant_array = grant_or_request_type.split(" ").sort
+          if ["id_token", "token"] == grant_array
+            Doorkeeper::Request::IdTokenAndToken
+          else
+            get_strategy_without_multitype(grant_or_request_type, available)
+          end
+        end
+        alias_method_chain :get_strategy, :multitype
+      end
+    end
+  end
+end
