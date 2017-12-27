2eed178328696aaf0d91a661abb8acca68b8c10a
Nicholas Hance <nhance@reenhanced.com>
2017-12-27 17:09:15 -0500

    Update request_extension.rb

--- lib/doorkeeper/openid_connect/request_extension.rb
+++ lib/doorkeeper/openid_connect/request_extension.rb
@@ -1,16 +1,15 @@
 module Doorkeeper
-    module Request
-      class << self
-        def get_strategy_with_multitype(grant_or_request_type, available)
-          grant_array = grant_or_request_type.split(" ").sort
-          if ["id_token", "token"] == grant_array
-            Doorkeeper::Request::IdTokenAndToken
-          else
-            get_strategy_without_multitype(grant_or_request_type, available)
-          end
+  module Request
+    class << self
+      def get_strategy_with_multitype(grant_or_request_type, available)
+        grant_array = grant_or_request_type.split(" ").sort
+        if ["id_token", "token"] == grant_array
+           Doorkeeper::Request::IdTokenAndToken
+        else
+          get_strategy_without_multitype(grant_or_request_type, available)
         end
-        alias_method_chain :get_strategy, :multitype
       end
+      alias_method_chain :get_strategy, :multitype
     end
   end
 end
