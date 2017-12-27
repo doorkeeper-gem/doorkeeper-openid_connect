51c1b42556d1b6fd3b247f5209f54fd7206fd15b
Nicholas Hance <nhance@reenhanced.com>
2017-12-27 16:11:41 -0500

    Update response_types_config.rb

--- lib/doorkeeper/openid_connect/response_types_config.rb
+++ lib/doorkeeper/openid_connect/response_types_config.rb
@@ -4,7 +4,8 @@ module Doorkeeper
       private def calculate_authorization_response_types
         types = super
         if grant_flows.include? 'implicit_oidc'
-          types << 'token id_token'
+          types << 'token id_token' # As per OpenID specification
+          types << 'id_token token' # But we can support either order
           types << 'token'
         end
         types
