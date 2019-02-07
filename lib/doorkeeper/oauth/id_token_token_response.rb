dc26e9a8e73a661e6b96fddc79e9ff1244907ba1
Matthew Eagar <meagar@gmail.com>
2019-02-06 20:48:07 -0500

    Separate id-token-token URL generation from params
    In order to make extending IdTokenTokenResponse easier, introduce a
    method that produces the params hash, and make `redirect_uri` consume
    that method. This means people wishing to extend the response with new
    parameters can prepend a module that defines `redirect_uri_params` and
    invokes `super`, rather than having to reimplement all of
    `redirect_uri`.

--- lib/doorkeeper/oauth/id_token_token_response.rb
+++ lib/doorkeeper/oauth/id_token_token_response.rb
@@ -2,14 +2,19 @@ module Doorkeeper
   module OAuth
     class IdTokenTokenResponse < IdTokenResponse
       def redirect_uri
-        Authorization::URIBuilder.uri_with_fragment(
-          pre_auth.redirect_uri,
+        Authorization::URIBuilder.uri_with_fragment(pre_auth.redirect_uri, redirect_uri_params)
+      end
+
+      private
+
+      def redirect_uri_params
+        {
           access_token: auth.token.token,
           token_type: auth.token.token_type,
           expires_in: auth.token.expires_in_seconds,
           state: pre_auth.state,
           id_token: id_token.as_jws_token
-        )
+        }
       end
     end
   end
