6e4264019fa5a9f2521849a3c0471ee867a8147b
Markus Koller <markus-koller@gmx.ch>
2019-02-15 10:50:45 +0100

    Support customization of redirect_uri_params in IdTokenResponse as well

--- lib/doorkeeper/oauth/id_token_response.rb
+++ lib/doorkeeper/oauth/id_token_response.rb
@@ -16,12 +16,17 @@ module Doorkeeper
       end
 
       def redirect_uri
-        Authorization::URIBuilder.uri_with_fragment(
-          pre_auth.redirect_uri,
+        Authorization::URIBuilder.uri_with_fragment(pre_auth.redirect_uri, redirect_uri_params)
+      end
+
+      private
+
+      def redirect_uri_params
+        {
           expires_in: auth.token.expires_in_seconds,
           state: pre_auth.state,
           id_token: id_token.as_jws_token
-        )
+        }
       end
     end
   end
--- lib/doorkeeper/oauth/id_token_token_response.rb
+++ lib/doorkeeper/oauth/id_token_token_response.rb
@@ -1,20 +1,13 @@
 module Doorkeeper
   module OAuth
     class IdTokenTokenResponse < IdTokenResponse
-      def redirect_uri
-        Authorization::URIBuilder.uri_with_fragment(pre_auth.redirect_uri, redirect_uri_params)
-      end
-
       private
 
       def redirect_uri_params
-        {
+        super.merge({
           access_token: auth.token.token,
-          token_type: auth.token.token_type,
-          expires_in: auth.token.expires_in_seconds,
-          state: pre_auth.state,
-          id_token: id_token.as_jws_token
-        }
+          token_type: auth.token.token_type
+        })
       end
     end
   end
