17245d74b43c058f51d3bdfbe3fdb01bf0325f36
Nat Budin <natbudin@gmail.com>
2019-09-16 14:46:00 -0700

    Check how many params OAuth::PreAuthorization#initialize expects and behave accordingly

--- lib/doorkeeper/openid_connect/oauth/pre_authorization.rb
+++ lib/doorkeeper/openid_connect/oauth/pre_authorization.rb
@@ -4,8 +4,19 @@ module Doorkeeper
       module PreAuthorization
         attr_reader :nonce
 
-        def initialize(server, client, attrs = {})
-          super
+        def initialize(server, client_or_attrs, attrs = {})
+          if client_or_attrs.is_a?(ActionController::Parameters)
+            attrs = client_or_attrs
+            client = nil
+          else
+            client = client_or_attrs
+          end
+
+          if OAuth::PreAuthorization.method(:initialize).parameters.include?([:req, :client])
+            super(server, client, attrs)
+          else
+            super(server, attrs)
+          end
           @nonce = attrs[:nonce]
         end
       end
