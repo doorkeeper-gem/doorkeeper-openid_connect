a5f67c3d6635e9768cfda9f244b34926a545a550
manojmj <manojmj92@gmail.com>
2019-12-06 12:58:40 +0530

    Use cascading deletes for deleting oauth_openid_requests upon deleting an access_grant
    Closes #82

--- spec/models/access_grant_spec.rb
+++ spec/models/access_grant_spec.rb
@@ -12,4 +12,15 @@ describe Doorkeeper::OpenidConnect::AccessGrant do
       dependent: :delete,
     })
   end
+
+  describe '#delete' do
+    it 'cascades to oauth_openid_requests' do
+      pending('Rails 6 changes - https://github.com/doorkeeper-gem/doorkeeper-openid_connect/issues/91')
+
+      access_grant = create(:access_grant, application: create(:application))
+      create(:openid_request, access_grant: access_grant)
+
+      expect { access_grant.delete }.to change(Doorkeeper::OpenidConnect::Request, :count).by(-1)
+    end
+  end
 end
