0b681cb389c1945d73362895c0fed9d17871b4eb
linhdangduy <dangduylinh.bk@gmail.com>
2021-03-20 17:30:18 +0900

    delete unnecessary support for dookeeper < v5.5

--- lib/doorkeeper/openid_connect/oauth/pre_authorization.rb
+++ lib/doorkeeper/openid_connect/oauth/pre_authorization.rb
@@ -7,12 +7,7 @@ module Doorkeeper
         attr_reader :nonce
 
         def initialize(server, attrs = {}, resource_owner = nil)
-          if (Doorkeeper::VERSION::MAJOR >= 5 && Doorkeeper::VERSION::MINOR >= 4) ||
-             Doorkeeper::VERSION::MAJOR >= 6
-            super
-          else
-            super(server, attrs)
-          end
+          super
           @nonce = attrs[:nonce]
         end
 
