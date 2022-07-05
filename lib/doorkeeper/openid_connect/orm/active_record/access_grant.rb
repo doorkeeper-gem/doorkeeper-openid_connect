97de4f11179ece0c37ac5a8b1822950524896f8f
nov <matake.nobukazu@moneyforward.co.jp>
2022-07-05 13:49:47 +0900

    allow to use custom doorkeeper access grant model

--- lib/doorkeeper/openid_connect/orm/active_record/access_grant.rb
+++ lib/doorkeeper/openid_connect/orm/active_record/access_grant.rb
@@ -7,6 +7,7 @@ module Doorkeeper
         base.class_eval do
           has_one :openid_request,
             class_name: 'Doorkeeper::OpenidConnect::Request',
+            foreign_key: 'access_grant_id',
             inverse_of: :access_grant,
             dependent: :delete
         end
